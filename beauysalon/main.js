7e5c5628f63bbc3b20ec901d94c9daab5dd3ae59
dusb <79008059+dusb@users.noreply.github.com>
2022-07-12 21:14:37 +0300

    popup edit

--- beauysalon/main.js
+++ beauysalon/main.js
@@ -29,19 +29,64 @@ $(document).ready(function () {
 
 
 // sheetdb
-const form = document.getElementById('form');
-form.addEventListener("submit", e => {
-  e.preventDefault();
-  fetch(form.action, {
-    method: "POST",
-    body: new FormData(form),
-  }).then(
-    response => response.json(),
-    form.reset(),
-    alert('Ваша заявка принята')
+// const form = document.getElementById('form');
+// form.addEventListener("submit", e => {
+//   e.preventDefault();
+//   fetch(form.action, {
+//     method: "POST",
+//     body: new FormData(form),
+//   }).then(
+//     response => response.json(),
+//     form.reset(),
+//     alert('Ваша заявка принята')
 
-  )
-});
+//   )
+// });
+
+
+const form = document.getElementById('form')
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwGILagAkK9r-yKzx_HgY5xXFMd0Ml1O0-aXHkENEd9Waj-J0SGY7OJ5ZLOUfDJ844F/exec'
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+
+  console.log(form)
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+  .then(
+        response => response.json(),
+        form.reset(),
+        alert('Ваша заявка принята')
+    
+      )
+    .catch(error => alert('Error!', error.message))
+})
+
+// showservice
+$('#select1').change(function(){
+  const val = $(this).val();
+  
+  
+  
+  if($('#' + val).length){
+    $('#step2').show();
+    $('#allshow').hide();
+     
+    $('#step2 select').hide();
+    
+    $('#' + val).show();
+    
+  }else if(val == 'all'){
+    
+    $('#allshow').show();
+    $('#step2').hide();
+  }
+})
+
+const url = 'https://script.google.com/macros/s/AKfycbwGILagAkK9r-yKzx_HgY5xXFMd0Ml1O0-aXHkENEd9Waj-J0SGY7OJ5ZLOUfDJ844F/exec';
+
+
+
+
 
 
 
